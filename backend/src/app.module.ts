6828207bf209c80a730a7ae5ba7b6447878fae0b
Silva404 <brenosilva0252@gmail.com>
2023-08-29 01:36:42 -0300

    rollback zod check for env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeeModule } from '@/UserInterface/Controller/coffee.module';
+import { envSchema } from './env';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
     CoffeeModule,
