fe82e700b3ea098488a8cd67f63ec1d1d80ece1b
Silva404 <brenosilva0252@gmail.com>
2023-08-29 01:34:33 -0300

    Remove zod env check

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoffeeModule } from '@/UserInterface/Controller/coffee.module';
-import { envSchema } from './env';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
     CoffeeModule,
